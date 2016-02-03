b8c456909407cf09403e4a75fe34d07d7d529f3e
Kunal <kunal94jha@gmail.com>
2016-02-03 16:50:46 +0100

    Combined tagging error unsolved

--- src/main/java/org/aksw/gscheck/error/CombinedTaggingError.java
+++ src/main/java/org/aksw/gscheck/error/CombinedTaggingError.java
@@ -1,6 +1,9 @@
 package org.aksw.gscheck.error;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.aksw.gerbil.dataset.DatasetConfiguration;
 import org.aksw.gerbil.dataset.impl.nif.NIFFileDatasetConfig;
@@ -14,17 +17,47 @@ public class CombinedTaggingError {
 			"C:/Users/Kunal/workspace/gerbil/gerbil_data/datasets/spotlight/dbpedia-spotlight-nif.ttl", false,
 			ExperimentType.A2KB);
 
-	static String text;
+	static String text, text1;
 
 	public static void main(String[] args) throws GerbilException {
 		List<Document> documents = DATASET.getDataset(ExperimentType.A2KB).getInstances();
+		int j = 0;
+
+		List<String> result = new ArrayList<String>();
 		for (Document doc : documents) {
 			text = doc.getText();
 			List<NamedEntity> entities = doc.getMarkings(NamedEntity.class);
+			NamedEntity a, b;
+			boolean s = false;
+			StringBuffer sb = new StringBuffer();
 			for (NamedEntity entity : entities) {
+				int i = entities.indexOf(entity);
+				if (i < entities.size()) {
+					text1 = text.substring(entity.getStartPosition() + entity.getLength(),
+							entities.get(i + 1).getStartPosition());
+					if (text1.equals("//s")) {
+						sb.append(text.substring(entity.getStartPosition(),
+								entities.get(i + 1).getStartPosition() + entities.get(i + 1).getLength()));
+
+						s = true;
+
+					} else {
+						s = false;
+					}
+
+				}
+
+				text1 = "";
+				
+				if(s==true)
+				{
+					result.add(sb.toString());
+					sb.delete(0,sb.length());
+				}
 			}
 
 		}
+		System.out.println(result);
 	}
 
 }
