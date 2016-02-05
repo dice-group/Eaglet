01e702f77963d18efb4cde60ed1e940ed4329036
Kunal <kunal94jha@gmail.com>
2016-02-05 16:19:17 +0100

    Stanford Tagger added.

--- src/main/java/org/aksw/gscheck/error/CombinedTaggingError.java
+++ src/main/java/org/aksw/gscheck/error/CombinedTaggingError.java
@@ -1,5 +1,4 @@
 package org.aksw.gscheck.error;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,37 +11,66 @@ import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.transfer.nif.data.NamedEntity;
 import org.aksw.gerbil.transfer.nif.data.StartPosBasedComparator;
 
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+
+import edu.stanford.nlp.trees.Tree;
+
 public class CombinedTaggingError {
-    private static final DatasetConfiguration DATASET = new NIFFileDatasetConfig("DBpedia",
-            "gerbil_data/datasets/spotlight/dbpedia-spotlight-nif.ttl", false, ExperimentType.A2KB);
+	private static final DatasetConfiguration DATASET = new NIFFileDatasetConfig("DBpedia",
+			"gerbil_data/datasets/spotlight/dbpedia-spotlight-nif.ttl", false, ExperimentType.A2KB);
 
-    public static void main(String[] args) throws GerbilException {
-        List<Document> documents = DATASET.getDataset(ExperimentType.A2KB).getInstances();
+	public static void main(String[] args) throws GerbilException {
+		List<Document> documents = DATASET.getDataset(ExperimentType.A2KB).getInstances();
 
-        String text, substring;
-        List<NamedEntity> entities;
-        NamedEntity a, b;
-        for (Document doc : documents) {
-            text = doc.getText();
-            entities = doc.getMarkings(NamedEntity.class);
-            Collections.sort(entities, new StartPosBasedComparator());
-            if (entities.size() > 0) {
-                b = entities.get(0);
-                for (int i = 1; i < entities.size(); ++i) {
-                    a = b;
-                    b = entities.get(i);
-                    // make sure that the entities are not overlapping
-                    if ((a.getStartPosition() + a.getLength()) <= b.getStartPosition()) {
-                        substring = text.substring(a.getStartPosition() + a.getLength(), b.getStartPosition());
-                        if (substring.matches("[\\s]*")) {
-                            System.out.println("I would connect two entities to a single large entity \""
-                                    + text.substring(a.getStartPosition(), b.getStartPosition() + b.getLength())
-                                    + "\".");
-                        }
-                    }
-                }
-            }
-        }
-    }
+		String substring;
+		List<NamedEntity> entities;
+		NamedEntity a, b;
+		for (Document doc : documents) {
+			String text = doc.getText();
 
+			LexicalizedParser lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
+			lp.setOptionFlags(new String[] { "-maxLength", "80", "-retainTmpSubcategories" });
+			// String sent="Ohio is located in America";
+			Tree parse;
+			parse = (Tree) lp.parse(text);
+			
+			 List<Tree> phraseList=new ArrayList<Tree>();
+			    for (Tree subtree: parse)
+			    {
+
+			      if(subtree.label().value().equals("NN"))
+			      {
+
+			        phraseList.add(subtree);
+			        System.out.println(subtree);
+
+			      }
+			    }
+
+			     
+			//List taggedWords = parse.taggedYield();
+
+//			System.out.println(taggedWords);
+
+			entities = doc.getMarkings(NamedEntity.class);
+			Collections.sort(entities, new StartPosBasedComparator());
+			if (entities.size() > 0) {
+				b = entities.get(0);
+				for (int i = 1; i < entities.size(); ++i) {
+					a = b;
+					b = entities.get(i);
+					// make sure that the entities are not overlapping
+					if ((a.getStartPosition() + a.getLength()) <= b.getStartPosition()) {
+						substring = text.substring(a.getStartPosition() + a.getLength(), b.getStartPosition());
+						if (substring.matches("[\\s]*")) {
+							System.out.println("I would connect two entities to a single large entity \""
+									+ text.substring(a.getStartPosition(), b.getStartPosition() + b.getLength())
+									+ "\".");
+						}
+					}
+				}
+			}
+		}
+
+	}
 }
