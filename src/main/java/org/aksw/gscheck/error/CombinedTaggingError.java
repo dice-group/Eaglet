22c9c8fa2074b6c9bf1ccab378a61e15bc489591
Kunal <kunal94jha@gmail.com>
2016-02-08 16:46:36 +0100

    Comparison of Stanford nlp result with combined tagging

--- src/main/java/org/aksw/gscheck/error/CombinedTaggingError.java
+++ src/main/java/org/aksw/gscheck/error/CombinedTaggingError.java
@@ -12,7 +12,6 @@ import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.transfer.nif.data.NamedEntity;
 import org.aksw.gerbil.transfer.nif.data.StartPosBasedComparator;
 
-import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
@@ -28,12 +27,14 @@ public class CombinedTaggingError {
 
 	public static void main(String[] args) throws GerbilException {
 		List<Document> documents = DATASET.getDataset(ExperimentType.A2KB).getInstances();
+		List<CoreLabel> eligible_makrings = new ArrayList<CoreLabel>();
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
 		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 		String substring;
 		List<NamedEntity> entities;
 		NamedEntity a, b;
+
 		for (Document doc : documents) {
 			String text = doc.getText();
 
@@ -41,51 +42,62 @@ public class CombinedTaggingError {
 			pipeline.annotate(document);
 
 			List<CoreLabel> tokens = document.get(TokensAnnotation.class);
-			int lastEnd = -1;
 			for (CoreLabel token : tokens) {
 				// we can get the token that has been marked inside the text
-				System.out.println(token.get(TextAnnotation.class));
-				// this is its position
-				System.out.println(token.get(PartOfSpeechAnnotation.class));
+				if (token.get(PartOfSpeechAnnotation.class).startsWith("N")
+						&& (!token.get(PartOfSpeechAnnotation.class).equals("NP"))
+						|| (token.get(PartOfSpeechAnnotation.class).startsWith("JJ"))) {
+					eligible_makrings.add(token);
+					// System.out.println(token.get(TextAnnotation.class));
+					// System.out.print(token.get(LemmaAnnotation.class));
+					// this is its position
+
+					// System.out.println(" " +
+					// token.get(PartOfSpeechAnnotation.class) + " " +
+					// token.beginPosition() + " "
+					// + token.endPosition());
+				}
+
 				// this is its lemma
-				System.out.println(token.get(LemmaAnnotation.class));
+				// System.out.println(" >>>");
 			}
-			/*
-			 * LexicalizedParser lp = LexicalizedParser.loadModel(
-			 * "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
-			 * lp.setOptionFlags(new String[] { "-maxLength", "80",
-			 * "-retainTmpSubcategories" }); // String sent=
-			 * "Ohio is located in America"; Tree parse; parse = (Tree)
-			 * lp.parse(text);
-			 * 
-			 * List<Tree> phraseList=new ArrayList<Tree>(); for (Tree subtree:
-			 * parse) {
-			 * 
-			 * if(subtree.label().value().startsWith("N")) {subtree.getSpan().
-			 * 
-			 * phraseList.add(subtree); System.out.println(subtree);
-			 * 
-			 * } }
-			 */
 
-			// List taggedWords = parse.taggedYield();
+			entities = doc.getMarkings(NamedEntity.class);
+			Collections.sort(entities, new StartPosBasedComparator());
+			if (entities.size() > 0) {
+				b = entities.get(0);
+				for (int i = 1; i < entities.size(); ++i) {
+					a = b;
+					b = entities.get(i); // make sure that the entities are not
+											// overlapping
+					if ((a.getStartPosition() + a.getLength()) <= b.getStartPosition()) {
+						substring = text.substring(a.getStartPosition() + a.getLength(), b.getStartPosition());
+						if (substring.matches("[\\s]*")) {
 
-			// System.out.println(taggedWords);
+							String[] arr = text.substring(a.getStartPosition(), b.getStartPosition() + b.getLength())
+									.split(" ");
+							for (String x : arr) {
+								for (CoreLabel z : eligible_makrings) {
+									if (z.get(TextAnnotation.class).equals(x)) {
+										System.out.println(
+												z.get(TextAnnotation.class) + " " + z.get(PartOfSpeechAnnotation.class)
+														+ " " + z.beginPosition() + " " + z.endPosition());
+									}
 
-			/*
-			 * entities = doc.getMarkings(NamedEntity.class);
-			 * Collections.sort(entities, new StartPosBasedComparator()); if
-			 * (entities.size() > 0) { b = entities.get(0); for (int i = 1; i <
-			 * entities.size(); ++i) { a = b; b = entities.get(i); // make sure
-			 * that the entities are not overlapping if ((a.getStartPosition() +
-			 * a.getLength()) <= b.getStartPosition()) { substring =
-			 * text.substring(a.getStartPosition() + a.getLength(),
-			 * b.getStartPosition()); if (substring.matches("[\\s]*")) {
-			 * System.out.println(
-			 * "I would connect two entities to a single large entity \"" +
-			 * text.substring(a.getStartPosition(), b.getStartPosition() +
-			 * b.getLength()) + "\"."); } } } }
-			 */
+									System.out.println("I would connect two entities to a single large entity \""
+											+ text.substring(a.getStartPosition(), b.getStartPosition() + b.getLength())
+											+ "\".");
+
+								}
+							}
+
+						}
+
+					}
+				}
+			}
+			// System.out.println("Document over!!");
+			eligible_makrings.clear();
 		}
 
 	}
