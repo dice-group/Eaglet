0de33c6e41fb380aab66121288482cf444fc589a
mroeder <roeder@informatik.uni-leipzig.de>
2016-03-17 18:33:08 +0100

    Fixed test.

--- src/main/java/org/aksw/gscheck/corrections/NamedEntityCorrections.java
+++ src/main/java/org/aksw/gscheck/corrections/NamedEntityCorrections.java
@@ -6,54 +6,98 @@ import java.util.Set;
 import org.aksw.gerbil.transfer.nif.data.NamedEntity;
 
 public class NamedEntityCorrections extends NamedEntity {
-	public enum Check {
-		INSERTED, DELETED, GOOD, NEED_TO_PAIR, OVERLAPS
-	}
+    public enum Check {
+        INSERTED, DELETED, GOOD, NEED_TO_PAIR, OVERLAPS
+    }
 
-	private Check result;
-	private NamedEntityCorrections partner;
-	private String entity_name;
-	private String doc;
+    private Check result;
+    private NamedEntityCorrections partner;
+    private String entity_name;
+    private String doc;
 
-	public String getEntity_name() {
-		return entity_name;
-	}
+    public String getEntity_name() {
+        return entity_name;
+    }
 
-	public void setEntity_name(String entity_name) {
-		this.entity_name = entity_name;
-	}
+    public void setEntity_name(String entity_name) {
+        this.entity_name = entity_name;
+    }
 
-	public String getDoc() {
-		return doc;
-	}
+    public String getDoc() {
+        return doc;
+    }
 
-	public void setDoc(String entity_text) {
-		this.doc = entity_text;
-	}
+    public void setDoc(String entity_text) {
+        this.doc = entity_text;
+    }
 
-	public NamedEntityCorrections(int startPosition, int length, String string) {
-		super(startPosition, length, string);
-		// TODO Auto-generated constructor stub
-		result = Check.GOOD;
-		partner = null;
+    public NamedEntityCorrections(int startPosition, int length, String string) {
+        super(startPosition, length, string);
+        // TODO Auto-generated constructor stub
+        result = Check.GOOD;
+        partner = null;
 
-	}
+    }
 
-	public NamedEntityCorrections getPartner() {
-		return partner;
-	}
+    public NamedEntityCorrections(int startPosition, int length, String string, Check result) {
+        super(startPosition, length, string);
+        this.result = result;
+    }
 
-	public void setPartner(NamedEntityCorrections pair_partner) {
+    public NamedEntityCorrections getPartner() {
+        return partner;
+    }
 
-		partner = pair_partner;
-	}
+    public void setPartner(NamedEntityCorrections pair_partner) {
 
-	public Check getResult() {
-		return result;
-	}
+        partner = pair_partner;
+    }
 
-	public void setResult(Check result) {
-		this.result = result;
-	}
+    public Check getResult() {
+        return result;
+    }
 
+    public void setResult(Check result) {
+        this.result = result;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((doc == null) ? 0 : doc.hashCode());
+        result = prime * result + ((entity_name == null) ? 0 : entity_name.hashCode());
+        result = prime * result + ((partner == null) ? 0 : partner.hashCode());
+        result = prime * result + ((this.result == null) ? 0 : this.result.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NamedEntityCorrections other = (NamedEntityCorrections) obj;
+        if (doc == null) {
+            if (other.doc != null)
+                return false;
+        } else if (!doc.equals(other.doc))
+            return false;
+        if (entity_name == null) {
+            if (other.entity_name != null)
+                return false;
+        } else if (!entity_name.equals(other.entity_name))
+            return false;
+        if (partner == null) {
+            if (other.partner != null)
+                return false;
+        } else if (!partner.equals(other.partner))
+            return false;
+        if (result != other.result)
+            return false;
+        return true;
+    }
 }
