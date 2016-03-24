fcc03a3fc1caa6b305362a29f2f32c97754f7fa2
mroeder <roeder@informatik.uni-leipzig.de>
2016-03-24 16:52:38 +0100

    Adapted the database schema.

--- src/main/resources/spring/database/schema/eaglet-schema.sql
+++ src/main/resources/spring/database/schema/eaglet-schema.sql
@@ -2,3 +2,9 @@ CREATE TABLE IF NOT EXISTS Users (
 id int PRIMARY KEY,
 name VARCHAR(50)
 );
+CREATE TABLE IF NOT EXISTIS Documents (
+userId int NOT NULL,
+documentUri VARCHAR(200) NOT NULL,
+fileName VARCHAR(100) NOT NULL,
+PRIMARY KEY (userId, documentUri)
+);
