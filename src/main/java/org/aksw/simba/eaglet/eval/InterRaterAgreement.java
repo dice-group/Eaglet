6af93944776d784e2c721ff2878e52acb27c38a6
mroeder <roeder@informatik.uni-leipzig.de>
2016-04-15 18:14:17 +0200

    Started the implementation of the interrater agreement.

--- src/main/java/org/aksw/simba/eaglet/eval/InterRaterAgreement.java
+++ src/main/java/org/aksw/simba/eaglet/eval/InterRaterAgreement.java
@@ -5,52 +5,151 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.RootPaneContainer;
+
+import org.aksw.gerbil.annotator.Annotator;
+import org.aksw.gerbil.annotator.AnnotatorConfiguration;
+import org.aksw.gerbil.dataset.Dataset;
+import org.aksw.gerbil.dataset.DatasetConfiguration;
+import org.aksw.gerbil.datatypes.AbstractAdapterConfiguration;
+import org.aksw.gerbil.datatypes.AdapterConfiguration;
+import org.aksw.gerbil.datatypes.ExperimentTaskConfiguration;
+import org.aksw.gerbil.datatypes.ExperimentType;
+import org.aksw.gerbil.datatypes.marking.ClassifiedSpanMeaning;
+import org.aksw.gerbil.datatypes.marking.MarkingClasses;
+import org.aksw.gerbil.evaluate.Evaluator;
+import org.aksw.gerbil.evaluate.EvaluatorFactory;
+import org.aksw.gerbil.evaluate.SubTaskEvaluator;
+import org.aksw.gerbil.evaluate.impl.ClassConsideringFMeasureCalculator;
+import org.aksw.gerbil.evaluate.impl.ClassifyingEvaluatorDecorator;
+import org.aksw.gerbil.exceptions.GerbilException;
 import org.aksw.gerbil.io.nif.DocumentListParser;
 import org.aksw.gerbil.io.nif.DocumentParser;
+import org.aksw.gerbil.matching.Matching;
+import org.aksw.gerbil.matching.MatchingsSearcher;
+import org.aksw.gerbil.matching.MatchingsSearcherFactory;
+import org.aksw.gerbil.matching.impl.ClassifiedMeaningMatchingsSearcher;
+import org.aksw.gerbil.matching.impl.CompoundMatchingsSearcher;
+import org.aksw.gerbil.matching.impl.MatchingsCounterImpl;
+import org.aksw.gerbil.matching.impl.clas.EmergingEntityMeaningClassifier;
+import org.aksw.gerbil.matching.impl.clas.UriBasedMeaningClassifier;
 import org.aksw.gerbil.transfer.nif.Document;
+import org.aksw.gerbil.transfer.nif.Marking;
+import org.aksw.gerbil.transfer.nif.MeaningSpan;
 import org.aksw.gerbil.transfer.nif.NIFTransferPrefixMapping;
-import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
+import org.aksw.gerbil.web.config.RootConfig;
 import org.aksw.simba.eaglet.annotator.AdaptedAnnotationParser;
-import org.aksw.simba.eaglet.entitytypemodify.EntityTypeChange;
+import org.aksw.simba.eaglet.entitytypemodify.EntityCheck;
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
-import arq.load;
-
 public class InterRaterAgreement {
 
-private static final Logger LOGGER = LoggerFactory.getLogger(InterRaterAgreement.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(InterRaterAgreement.class);
 
-private static final String DATASET_FILES[] = new String[] { "eaglet_data/result_pipe/kore50-nif-result-nif.ttl" };
+    private static final int USER_IDS[] = new int[] { 1, 6 };
+    private static final String USER_OUTPUT_FOLDER = "eaglet_data/result_user";
 
     public static void main(String[] args) {
-        loadDocuments();
+        InterRaterAgreement raterAgreement = new InterRaterAgreement();
+        raterAgreement.run(USER_IDS, new File(USER_OUTPUT_FOLDER));
     }
 
-    protected static List<Document> loadDocuments() {
-        List<Document> loadedDocuments = new ArrayList<Document>();
-        for (int i = 0; i < DATASET_FILES.length; ++i) {
-            try {
-                Model nifModel = ModelFactory.createDefaultModel();
-                nifModel.setNsPrefixes(NIFTransferPrefixMapping.getInstance());
-                FileInputStream fin = new FileInputStream(new File(DATASET_FILES[i]));
-                nifModel.read(fin, "", "TTL");
-                fin.close();
-                DocumentListParser parser = new DocumentListParser(new DocumentParser(new AdaptedAnnotationParser()));
-                List<Document> documents = parser.parseDocuments(nifModel);
-                for (Document document : documents) {
-                    loadedDocuments.add(new DocumentImpl(document.getText(), document.getDocumentURI(),
-                            EntityTypeChange.changeType(document)));
-                }
+    private ExtendedEvaluatorFactory factory = new ExtendedEvaluatorFactory();
 
-                // loadedDocuments.addAll(CheckerPipeline.readDocuments(DATASET_FILES[i]));
-            } catch (Exception e) {
-                LOGGER.error("Couldn't load the dataset!", e);
+    public void run(int[] userIds, File userOutputFolder) {
+        List<List<Document>> documents = loadDocuments(userIds, userOutputFolder);
+        for (int i = 0; i < userIds.length; ++i) {
+            for (int j = i + 1; j < userIds.length; ++j) {
+                LOGGER.info("Comparing user {} and {}.", userIds[i], userIds[j]);
+                evaluate(documents.get(i), documents.get(j));
             }
         }
+    }
+
+    protected List<List<Document>> loadDocuments(int userIds[], File userOutputFolder) {
+        List<List<Document>> loadedDocuments = new ArrayList<List<Document>>();
+        List<Document> userDocuments = null;
+        String expectedPrefix;
+        for (int i = 0; i < userIds.length; ++i) {
+            userDocuments = new ArrayList<Document>();
+            expectedPrefix = "result-" + userIds[i];
+            for (File file : userOutputFolder.listFiles()) {
+                if (file.getName().startsWith(expectedPrefix))
+                    try {
+                        Model nifModel = ModelFactory.createDefaultModel();
+                        nifModel.setNsPrefixes(NIFTransferPrefixMapping.getInstance());
+                        FileInputStream fin = new FileInputStream(file);
+                        nifModel.read(fin, "", "TTL");
+                        fin.close();
+                        DocumentListParser parser = new DocumentListParser(
+                                new DocumentParser(new AdaptedAnnotationParser()));
+                        userDocuments.addAll(parser.parseDocuments(nifModel));
+                    } catch (Exception e) {
+                        LOGGER.error("Couldn't load the dataset!", e);
+                    }
+            }
+            loadedDocuments.add(userDocuments);
+        }
         return loadedDocuments;
+    }
+
+    private void evaluate(List<Document> list, List<Document> list2) {
+
+    }
+
+    protected class ExtendedEvaluatorFactory extends EvaluatorFactory {
+
+        @SuppressWarnings("unchecked")
+        private List<Evaluator<EntityCheck>> createEvaluator(String name1, String name2) {
+            List<Evaluator<EntityCheck>> evaluators = new ArrayList<Evaluator<EntityCheck>>();
+
+            MatchingsSearcher<ClassifiedSpanMeaning> searcher = (MatchingsSearcher<ClassifiedSpanMeaning>) MatchingsSearcherFactory
+                    .createSpanMatchingsSearcher(Matching.STRONG_ANNOTATION_MATCH);
+//            evaluators.add(new ClassifyingEvaluatorDecorator<MeaningSpan, ClassifiedSpanMeaning>(
+//                    new ClassConsideringFMeasureCalculator<ClassifiedSpanMeaning>(
+//                            new MatchingsCounterImpl<ClassifiedSpanMeaning>(
+//                                    new CompoundMatchingsSearcher<ClassifiedSpanMeaning>(searcher,
+//                                            new ClassifiedMeaningMatchingsSearcher<ClassifiedSpanMeaning>())),
+//                            MarkingClasses.IN_KB, MarkingClasses.EE, MarkingClasses.GS_IN_KB),
+//                    new UriBasedMeaningClassifier<ClassifiedSpanMeaning>(this.globalClassifier, MarkingClasses.IN_KB),
+//                    new EmergingEntityMeaningClassifier<ClassifiedSpanMeaning>()));
+
+            ExperimentTaskConfiguration subTaskConfig;
+            subTaskConfig = new ExperimentTaskConfiguration(new ConfigurationImpl(name1, false, ExperimentType.ERec),
+                    new ConfigurationImpl(name2, false, ExperimentType.ERec), ExperimentType.ERec,
+                    Matching.STRONG_ANNOTATION_MATCH);
+            evaluators.add(
+                    new SubTaskEvaluator<>(subTaskConfig, createEvaluator(ExperimentType.ERec, subTaskConfig, null)));
+            subTaskConfig = new ExperimentTaskConfiguration(new ConfigurationImpl(name1, false, ExperimentType.C2KB),
+                    new ConfigurationImpl(name2, false, ExperimentType.C2KB), ExperimentType.C2KB,
+                    Matching.STRONG_ENTITY_MATCH);
+            evaluators.add(
+                    new SubTaskEvaluator<>(subTaskConfig, createEvaluator(ExperimentType.C2KB, subTaskConfig, null)));
+            return evaluators;
+        }
+
+    }
+
+    protected class ConfigurationImpl extends AbstractAdapterConfiguration
+            implements DatasetConfiguration, AnnotatorConfiguration {
+
+        public ConfigurationImpl(String name, boolean couldBeCached, ExperimentType applicableForExperiment) {
+            super(name, couldBeCached, applicableForExperiment);
+        }
+
+        @Override
+        public Annotator getAnnotator(ExperimentType type) throws GerbilException {
+            return null;
+        }
+
+        @Override
+        public Dataset getDataset(ExperimentType experimentType) throws GerbilException {
+            return null;
+        }
     }
 }
