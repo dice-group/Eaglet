5793f1f01d24370511232c599fc3cbe0549ec6dd
Kunal <kunal94jha@gmail.com>
2016-04-28 18:03:16 +0200

    Resources added

--- src/main/java/org/aksw/simba/eaglet/annotator/AdaptedAnnotationParser.java
+++ src/main/java/org/aksw/simba/eaglet/annotator/AdaptedAnnotationParser.java
@@ -36,6 +36,7 @@ import org.aksw.gerbil.transfer.nif.vocabulary.NIF;
 import org.aksw.simba.eaglet.entitytypemodify.EntityCheck;
 import org.aksw.simba.eaglet.entitytypemodify.NamedEntityCorrections;
 import org.aksw.simba.eaglet.entitytypemodify.NamedEntityCorrections.Check;
+import org.aksw.simba.eaglet.entitytypemodify.NamedEntityCorrections.ErrorType;
 import org.aksw.simba.eaglet.vocab.EAGLET;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,167 +49,187 @@ import com.hp.hpl.jena.vocabulary.RDF;
 
 public class AdaptedAnnotationParser extends AnnotationParser {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AdaptedAnnotationParser.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AdaptedAnnotationParser.class);
 
-    private boolean removeUsedProperties;
+	private boolean removeUsedProperties;
 
-    public AdaptedAnnotationParser() {
-        this(false);
-    }
+	public AdaptedAnnotationParser() {
+		this(false);
+	}
 
-    public AdaptedAnnotationParser(boolean removeUsedProperties) {
-        super(removeUsedProperties);
-        this.removeUsedProperties = removeUsedProperties;
-    }
+	public AdaptedAnnotationParser(boolean removeUsedProperties) {
+		super(removeUsedProperties);
+		this.removeUsedProperties = removeUsedProperties;
+	}
 
-    public void parseAnnotations(Model nifModel, Document document, Resource documentResource) {
-        // get the annotations from the model
-        List<Marking> markings = document.getMarkings();
-        ResIterator resIter = nifModel.listSubjectsWithProperty(NIF.referenceContext, documentResource);
-        Resource annotationResource;
-        int start, end;
-        Set<String> entityUris;
-        double confidence;
-        NodeIterator nodeIter;
-        while (resIter.hasNext()) {
-            annotationResource = resIter.next();
-            start = end = -1;
-            nodeIter = nifModel.listObjectsOfProperty(annotationResource, NIF.beginIndex);
-            if (nodeIter.hasNext()) {
-                start = nodeIter.next().asLiteral().getInt();
-            }
-            nodeIter = nifModel.listObjectsOfProperty(annotationResource, NIF.endIndex);
-            if (nodeIter.hasNext()) {
-                end = nodeIter.next().asLiteral().getInt();
-            }
-            if ((start >= 0) && (end >= 0)) {
-                nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taIdentRef);
-                if (nodeIter.hasNext()) {
-                    entityUris = new HashSet<String>();
-                    while (nodeIter.hasNext()) {
-                        entityUris.add(nodeIter.next().toString());
-                    }
-                    nodeIter = nifModel.listObjectsOfProperty(annotationResource, EAGLET.hasCheckResult);
-                    if (nodeIter.hasNext()) {
-                        Check result = parseCheckResult(nodeIter.next().asResource());
-                        markings.add(new NamedEntityCorrections(start, end - start, entityUris, result));
-                    } else {
-                        nodeIter = nifModel.listObjectsOfProperty(annotationResource, EAGLET.isNamedEntity);
-                        if (nodeIter.hasNext()) {
-                            boolean namedentity = nodeIter.next().asLiteral().getBoolean();
-                            markings.add(new EntityCheck(start, end - start, entityUris, namedentity));
-                        } else {
-                            nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taClassRef);
-                            if (nodeIter.hasNext()) {
-                                Set<String> types = new HashSet<String>();
-                                while (nodeIter.hasNext()) {
-                                    types.add(nodeIter.next().toString());
-                                }
-                            } else {
-                                nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taConfidence);
-                                if (nodeIter.hasNext()) {
-                                    confidence = nodeIter.next().asLiteral().getDouble();
-                                    markings.add(addTypeInformationIfPossible(
-                                            new ScoredNamedEntity(start, end - start, entityUris, confidence),
-                                            nifModel));
-                                } else {
-                                    // It has been disambiguated without a
-                                    // confidence
-                                    markings.add(addTypeInformationIfPossible(
-                                            new NamedEntity(start, end - start, entityUris), nifModel));
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    // It is a named entity that hasn't been disambiguated
-                    markings.add(new SpanImpl(start, end - start));
-                }
-                // FIXME scored Span is missing
-            } else {
-                LOGGER.warn("Found an annotation resource (\"" + annotationResource.getURI()
-                        + "\") without a start or end index. This annotation will be ignored.");
-            }
-            if (removeUsedProperties) {
-                nifModel.removeAll(annotationResource, null, null);
-            }
-        }
+	public void parseAnnotations(Model nifModel, Document document, Resource documentResource) {
+		// get the annotations from the model
+		List<Marking> markings = document.getMarkings();
+		ResIterator resIter = nifModel.listSubjectsWithProperty(NIF.referenceContext, documentResource);
+		Resource annotationResource;
+		int start, end;
+		Set<String> entityUris;
+		double confidence;
+		NodeIterator nodeIter;
+		while (resIter.hasNext()) {
+			annotationResource = resIter.next();
+			start = end = -1;
+			nodeIter = nifModel.listObjectsOfProperty(annotationResource, NIF.beginIndex);
+			if (nodeIter.hasNext()) {
+				start = nodeIter.next().asLiteral().getInt();
+			}
+			nodeIter = nifModel.listObjectsOfProperty(annotationResource, NIF.endIndex);
+			if (nodeIter.hasNext()) {
+				end = nodeIter.next().asLiteral().getInt();
+			}
+			if ((start >= 0) && (end >= 0)) {
+				nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taIdentRef);
+				if (nodeIter.hasNext()) {
+					entityUris = new HashSet<String>();
+					while (nodeIter.hasNext()) {
+						entityUris.add(nodeIter.next().toString());
+					}
+					nodeIter = nifModel.listObjectsOfProperty(annotationResource, EAGLET.hasCheckResult);
+					if (nodeIter.hasNext()) {
+						Check result = parseCheckResult(nodeIter.next().asResource());
+						markings.add(new NamedEntityCorrections(start, end - start, entityUris, result));
+					} else {
+						nodeIter = nifModel.listObjectsOfProperty(annotationResource, EAGLET.isNamedEntity);
+						if (nodeIter.hasNext()) {
+							boolean namedentity = nodeIter.next().asLiteral().getBoolean();
+							markings.add(new EntityCheck(start, end - start, entityUris, namedentity));
+						} else {
+							nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taClassRef);
+							if (nodeIter.hasNext()) {
+								Set<String> types = new HashSet<String>();
+								while (nodeIter.hasNext()) {
+									types.add(nodeIter.next().toString());
+								}
+							} else {
+								nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taConfidence);
+								if (nodeIter.hasNext()) {
+									confidence = nodeIter.next().asLiteral().getDouble();
+									markings.add(addTypeInformationIfPossible(
+											new ScoredNamedEntity(start, end - start, entityUris, confidence),
+											nifModel));
+								} else {
+									// It has been disambiguated without a
+									// confidence
+									markings.add(addTypeInformationIfPossible(
+											new NamedEntity(start, end - start, entityUris), nifModel));
+								}
+							}
+						}
+					}
+				} else {
+					// It is a named entity that hasn't been disambiguated
+					markings.add(new SpanImpl(start, end - start));
+				}
+				// FIXME scored Span is missing
+			} else {
+				LOGGER.warn("Found an annotation resource (\"" + annotationResource.getURI()
+						+ "\") without a start or end index. This annotation will be ignored.");
+			}
+			if (removeUsedProperties) {
+				nifModel.removeAll(annotationResource, null, null);
+			}
+		}
 
-        NodeIterator annotationIter = nifModel.listObjectsOfProperty(documentResource, NIF.topic);
-        while (annotationIter.hasNext()) {
-            annotationResource = annotationIter.next().asResource();
-            nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taIdentRef);
-            if (nodeIter.hasNext()) {
-                entityUris = new HashSet<String>();
-                while (nodeIter.hasNext()) {
-                    entityUris.add(nodeIter.next().toString());
-                }
-                nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taConfidence);
-                if (nodeIter.hasNext()) {
-                    confidence = nodeIter.next().asLiteral().getDouble();
-                    markings.add(new ScoredAnnotation(entityUris, confidence));
-                } else {
-                    markings.add(new Annotation(entityUris));
-                }
-            }
-        }
-    }
+		NodeIterator annotationIter = nifModel.listObjectsOfProperty(documentResource, NIF.topic);
+		while (annotationIter.hasNext()) {
+			annotationResource = annotationIter.next().asResource();
+			nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taIdentRef);
+			if (nodeIter.hasNext()) {
+				entityUris = new HashSet<String>();
+				while (nodeIter.hasNext()) {
+					entityUris.add(nodeIter.next().toString());
+				}
+				nodeIter = nifModel.listObjectsOfProperty(annotationResource, ITSRDF.taConfidence);
+				if (nodeIter.hasNext()) {
+					confidence = nodeIter.next().asLiteral().getDouble();
+					markings.add(new ScoredAnnotation(entityUris, confidence));
+				} else {
+					markings.add(new Annotation(entityUris));
+				}
+			}
+		}
+	}
 
-    private Check parseCheckResult(Resource resource) {
-        if (EAGLET.Inserted.equals(resource)) {
-            return Check.INSERTED;
-        } else if (EAGLET.Deleted.equals(resource)) {
-            return Check.DELETED;
-        } else if (EAGLET.Good.equals(resource)) {
-            return Check.GOOD;
-        } else if (EAGLET.NeedToPair.equals(resource)) {
-            return Check.NEED_TO_PAIR;
-        } else if (EAGLET.Overlaps.equals(resource)) {
-            return Check.OVERLAPS;
-        } else if (EAGLET.Completed.equals(resource)) {
-            return Check.COMPLETED;
-        } else if (EAGLET.InvalidUri.equals(resource)) {
-            return Check.INVALID_URI;
-        } else if (EAGLET.OutdatedUri.equals(resource)) {
-            return Check.OUTDATED_URI;
-        } else if (EAGLET.DisambiguationUri.equals(resource)) {
-            return Check.DISAMBIG_URI;
-        } else {
-            LOGGER.error("Got an unknown matching type: " + resource);
-            return null;
-        }
-    }
+	private Check parseCheckResult(Resource resource) {
+		if (EAGLET.Inserted.equals(resource)) {
+			return Check.INSERTED;
+		} else if (EAGLET.Deleted.equals(resource)) {
+			return Check.DELETED;
+		} else if (EAGLET.Good.equals(resource)) {
+			return Check.GOOD;
+		} else if (EAGLET.NeedToPair.equals(resource)) {
+			return Check.NEED_TO_PAIR;
+		} else if (EAGLET.Overlaps.equals(resource)) {
+			return Check.OVERLAPS;
+		} else if (EAGLET.Completed.equals(resource)) {
+			return Check.COMPLETED;
+		} else if (EAGLET.InvalidUri.equals(resource)) {
+			return Check.INVALID_URI;
+		} else if (EAGLET.OutdatedUri.equals(resource)) {
+			return Check.OUTDATED_URI;
+		} else if (EAGLET.DisambiguationUri.equals(resource)) {
+			return Check.DISAMBIG_URI;
+		} else {
+			LOGGER.error("Got an unknown matching type: " + resource);
+			return null;
+		}
+	}
 
-    private MeaningSpan addTypeInformationIfPossible(NamedEntity ne, Model nifModel) {
-        TypedNamedEntity typedNE = new TypedNamedEntity(ne.getStartPosition(), ne.getLength(), ne.getUris(),
-                new HashSet<String>());
-        addTypeInformation(typedNE, nifModel);
-        if (typedNE.getTypes().size() > 0) {
-            return typedNE;
-        } else {
-            return ne;
-        }
-    }
+	private ErrorType parseErroResult(Resource resource) {
+		if (EAGLET.Overlapping.equals(resource)) {
+			return ErrorType.OVERLAPPING;
+		} else if (EAGLET.Combined.equals(resource)) {
+			return ErrorType.COMBINED;
+		} else if (EAGLET.Erratic.equals(resource)) {
+			return ErrorType.ERRATIC;
+		} else if (EAGLET.WrongPos.equals(resource)) {
+			return ErrorType.WRONGPOSITION;
+		} else if (EAGLET.LongDesc.equals(resource)) {
+			return ErrorType.LONGDESC;
+		} else if (EAGLET.Uri.equals(resource)) {
+			return ErrorType.URI;
 
-    private MeaningSpan addTypeInformationIfPossible(ScoredNamedEntity ne, Model nifModel) {
-        ScoredTypedNamedEntity typedNE = new ScoredTypedNamedEntity(ne.getStartPosition(), ne.getLength(), ne.getUris(),
-                new HashSet<String>(), ne.getConfidence());
-        addTypeInformation(typedNE, nifModel);
-        if (typedNE.getTypes().size() > 0) {
-            return typedNE;
-        } else {
-            return ne;
-        }
-    }
+		} else {
+			LOGGER.error("Got an unknown matching type: " + resource);
+			return null;
+		}
+	}
 
-    private TypedNamedEntity addTypeInformation(TypedNamedEntity typedNE, Model nifModel) {
-        for (String uri : typedNE.getUris()) {
-            NodeIterator nodeIter = nifModel.listObjectsOfProperty(nifModel.getResource(uri), RDF.type);
-            Set<String> types = typedNE.getTypes();
-            while (nodeIter.hasNext()) {
-                types.add(nodeIter.next().asResource().getURI());
-            }
-        }
-        return typedNE;
-    }
+	private MeaningSpan addTypeInformationIfPossible(NamedEntity ne, Model nifModel) {
+		TypedNamedEntity typedNE = new TypedNamedEntity(ne.getStartPosition(), ne.getLength(), ne.getUris(),
+				new HashSet<String>());
+		addTypeInformation(typedNE, nifModel);
+		if (typedNE.getTypes().size() > 0) {
+			return typedNE;
+		} else {
+			return ne;
+		}
+	}
+
+	private MeaningSpan addTypeInformationIfPossible(ScoredNamedEntity ne, Model nifModel) {
+		ScoredTypedNamedEntity typedNE = new ScoredTypedNamedEntity(ne.getStartPosition(), ne.getLength(), ne.getUris(),
+				new HashSet<String>(), ne.getConfidence());
+		addTypeInformation(typedNE, nifModel);
+		if (typedNE.getTypes().size() > 0) {
+			return typedNE;
+		} else {
+			return ne;
+		}
+	}
+
+	private TypedNamedEntity addTypeInformation(TypedNamedEntity typedNE, Model nifModel) {
+		for (String uri : typedNE.getUris()) {
+			NodeIterator nodeIter = nifModel.listObjectsOfProperty(nifModel.getResource(uri), RDF.type);
+			Set<String> types = typedNE.getTypes();
+			while (nodeIter.hasNext()) {
+				types.add(nodeIter.next().asResource().getURI());
+			}
+		}
+		return typedNE;
+	}
 }
--- src/main/java/org/aksw/simba/eaglet/vocab/EAGLET.java
+++ src/main/java/org/aksw/simba/eaglet/vocab/EAGLET.java
@@ -17,6 +17,7 @@
 package org.aksw.simba.eaglet.vocab;
 
 import org.aksw.simba.eaglet.entitytypemodify.NamedEntityCorrections.Check;
+import org.aksw.simba.eaglet.entitytypemodify.NamedEntityCorrections.ErrorType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,65 +27,92 @@ import com.hp.hpl.jena.rdf.model.ResourceFactory;
 
 public class EAGLET {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EAGLET.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(EAGLET.class);
 
-    protected static final String uri = "http://gerbil.aksw.org/eaglet/vocab#";
+	protected static final String uri = "http://gerbil.aksw.org/eaglet/vocab#";
 
-    /**
-     * returns the URI for this schema
-     * 
-     * @return the URI for this schema
-     */
-    public static String getURI() {
-        return uri;
-    }
+	/**
+	 * returns the URI for this schema
+	 * 
+	 * @return the URI for this schema
+	 */
+	public static String getURI() {
+		return uri;
+	}
 
-    protected static final Resource resource(String local) {
-        return ResourceFactory.createResource(uri + local);
-    }
+	protected static final Resource resource(String local) {
+		return ResourceFactory.createResource(uri + local);
+	}
 
-    protected static final Property property(String local) {
-        return ResourceFactory.createProperty(uri, local);
-    }
+	protected static final Property property(String local) {
+		return ResourceFactory.createProperty(uri, local);
+	}
 
-    public static final Resource Inserted = resource("Inserted");
-    public static final Resource Deleted = resource("Deleted");
-    public static final Resource Good = resource("Good");
-    public static final Resource NeedToPair = resource("NeedToPair");
-    public static final Resource Overlaps = resource("Overlaps");
-    public static final Resource Completed = resource("Completed");
-    public static final Resource InvalidUri = resource("InvalidUri");
-    public static final Resource OutdatedUri = resource("OutdatedUri");
-    public static final Resource DisambiguationUri = resource("DisambiguationUri");
+	public static final Resource Inserted = resource("Inserted");
+	public static final Resource Deleted = resource("Deleted");
+	public static final Resource Good = resource("Good");
+	public static final Resource NeedToPair = resource("NeedToPair");
+	public static final Resource Overlaps = resource("Overlaps");
+	public static final Resource Completed = resource("Completed");
+	public static final Resource InvalidUri = resource("InvalidUri");
+	public static final Resource OutdatedUri = resource("OutdatedUri");
+	public static final Resource DisambiguationUri = resource("DisambiguationUri");
+	public static final Resource Combined = resource("Combined");
+	public static final Resource Overlapping = resource("Overlapping");
+	public static final Resource Erratic = resource("Combined");
+	public static final Resource WrongPos = resource("WrongPos");
+	public static final Resource LongDesc = resource("LongDesc");
+	public static final Resource Uri = resource("Uri");
 
-    public static final Property hasCheckResult = property("hasCheckResult");
-    public static final Property hasPairPartner = property("hasPairPartner");
-    public static final Property isNamedEntity = property("isNamedEntity");
+	public static final Property hasErrorType = property("hasErrorType");
+	public static final Property hasCheckResult = property("hasCheckResult");
+	public static final Property hasPairPartner = property("hasPairPartner");
+	public static final Property isNamedEntity = property("isNamedEntity");
 
-    public static Resource getCheckResult(Check checkResult) {
-        switch (checkResult) {
-        case INSERTED:
-            return Inserted;
-        case DELETED:
-            return Deleted;
-        case GOOD:
-            return Good;
-        case NEED_TO_PAIR:
-            return NeedToPair;
-        case OVERLAPS:
-            return Overlaps;
-        case COMPLETED:
-            return Completed;
-        case INVALID_URI:
-            return InvalidUri;
-        case OUTDATED_URI:
-            return OutdatedUri;
-        case DISAMBIG_URI:
-            return DisambiguationUri;
+	public static Resource getErrorType(ErrorType errorkind) {
+		switch (errorkind) {
+		case OVERLAPPING:
+			return Overlapping;
+		case COMBINED:
+			return Combined;
+		case ERRATIC:
+			return Erratic;
+		case LONGDESC:
+			return LongDesc;
+		case URI:
+			return Uri;
+		case WRONGPOSITION:
+			return WrongPos;
 
-        }
-        LOGGER.error("Got an unknown matching type: " + checkResult.name());
-        return null;
-    }
+		}
+		LOGGER.error("Got an unknown matching type: " + errorkind.name());
+		return null;
+	}
+
+	public static Resource getCheckResult(Check checkResult) {
+		switch (checkResult) {
+		case INSERTED:
+			return Inserted;
+		case DELETED:
+			return Deleted;
+		case GOOD:
+			return Good;
+		case NEED_TO_PAIR:
+			return NeedToPair;
+		case OVERLAPS:
+			return Overlaps;
+		case COMPLETED:
+			return Completed;
+		case INVALID_URI:
+			return InvalidUri;
+		case OUTDATED_URI:
+			return OutdatedUri;
+		case DISAMBIG_URI:
+			return DisambiguationUri;
+
+		}
+		LOGGER.error("Got an unknown matching type: " + checkResult.name());
+		return null;
+	}
 
 }
